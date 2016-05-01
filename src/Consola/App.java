29cdd274e6c05e1303ddf162cff1cc7809db7b54
JLGG <hotolete@gmail.com>
2016-05-01 16:26:40 +0200

    Creo objeto CentroVeterinario en clase App, y le paso objetos gato y perro.

--- src/Consola/App.java
+++ src/Consola/App.java
@@ -1,6 +1,7 @@
 package Consola;
 
 import fauna.Animal;
+import fauna.CentroVeterinario;
 import fauna.Gato;
 import fauna.Perro;
 
@@ -9,5 +10,10 @@ public class App {
 	public static void main(String[] args) {
 		Gato g = new Gato("Misha", 3.0f); 
 		Perro p = new Perro("Lassie", 20.0f); 
+		
+		//creo un CentroVeterinario llamado v
+		CentroVeterinario v = new CentroVeterinario();  
+		v.addAnimal(g); //le añado el gato g al CentroVeterinario.
+		v.addAnimal(p); //le añado el perro
 	}
 }

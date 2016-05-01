92edcca64554cd10ea92a97847ac301846fe35ce
JLGG <hotolete@gmail.com>
2016-05-01 15:46:03 +0200

    Creo clase Gato hija de Animal, le pongo un constructor sin parámetros y otro con parámetros.

---
+++ src/fauna/Gato.java
@@ -0,0 +1,13 @@
+package fauna;
+
+public class Gato extends Animal {
+
+	public Gato() {
+		this("", 0.0f); 
+		// TODO Auto-generated constructor stub
+	}
+
+	public Gato(String nombre, float peso) {
+		super(nombre, peso);
+	}
+}
